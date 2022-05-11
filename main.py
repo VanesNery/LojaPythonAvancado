7b2a85273c56f64e626f5ef396cacb283f1dd1c0
Vanessa Nery <nery_van@hotmail.com>
2022-05-11 14:09:46 -0300

    Herança de classes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #! usr/local/bin/python3
+from datetime import datetime
 
 
 class Pessoa:
@@ -11,4 +12,31 @@ class Pessoa:
         return f'Nome: {self.nome} e sua idade é: {self.idade}'
     
     def is_adulto(self):
+        if self.idade >= 20:
+            return f'É Adulto'
+        else:
+            return f'É criança'
+
+
+class Cliente(Pessoa):
+    compras = []
+    
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+    
+    def registrar_comprar(self, compra):
         pass
+    
+    def get_data_ultima_compra(self):
+        pass
+    
+    def total_compras(self):
+        pass
+
+
+class Vendedor(Pessoa):
+    
+    def __init__(self, nome, idade, salario):
+        super().__init__(nome, idade)
+        self.salario = salario
+
